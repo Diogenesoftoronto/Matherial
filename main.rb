a7aee44fc96d93b0509e78abb15d53cc9209ef2e
Diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-04 02:13:49 +0000

    this is my commit

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
 require '/'
+
+#  Game loop with a welcome message and flag that will determine if the game is over
+# or not. This is a game where the player has to answer the simple addition question correctly. if they guess incorrectly then
+#  the next player is given a chance
+do 
+
+
+
+end while (gaming)  
