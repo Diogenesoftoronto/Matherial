034498653f88e38dd87c0506acb399f17c40341d
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-06 05:23:30 -0500

    add an exit message

--- main.rb
+++ main.rb
@@ -3,8 +3,9 @@ require '/'
 #  Game loop with a welcome message and flag that will determine if the game is over
 # or not. This is a game where the player has to answer the simple addition question correctly. if they guess incorrectly then
 #  the next player is given a chance
-do 
+while (gaming)
 
 
 
-end while (gaming)  
+end   
+puts "Thanks for playing!"
