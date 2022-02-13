c3b47b3f492aa057a1d69cc1f3eb7f85a711077c
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-13 18:15:49 -0500

    add results variables and functionality

--- src/classes/player_class.rb
+++ src/classes/player_class.rb
@@ -3,16 +3,31 @@ class Player
   @name = 'Anon'
   @score = 0
   @turn = false
+  @results = [] 
+  @response_to_question = {}
+  @amount_of_questions = @results.length
   # may add wins, losses, and ties later.
   # may add difficulty later.
   def initialize(name)
     @name = name
   end
-  attr_accessor :name, :score, :turn
+  
+  attr_accessor :name, :score, :turn, :results
+  
   def player_state
-    puts "Player: #{@@name} Score: #{@@score} Turn: #{@@turn}"
+    if self.turn === true
+      puts "Player: #{@name} Score: #{@score} Turn: #{@turn}"
+    end
   end
+
+  def add_to_results
+    if self.results[0] !== {} 
+      @results.push(@response_to_question)
+    end
+  end
+
   def calculate_score(time, correct_answers, amount_of_questions)
-    @@score = (amount_of_questions*correct_answers) - time
+    @score = (amount_of_questions*correct_answers) - time
   end
+
 end
